26557c471168bde7f9e7c93677213301c5561879
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-11 16:07:00 +0530

    adding arc to the site + express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const express = require('express');
+const app = express()
+app.set('json spaces', 1);
+app.use('/public', express.static('public'))
+app.use(bodyParser.json());
+
+app.get('/arc-sw.js', function (req, res) {
+	res.sendFile('/arc-sw.js', {root: './routes'})
+});
+
+app.listen(3000)
+console.log('Server is now ready on port: 3000')
