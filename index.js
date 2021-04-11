60e89a0b8d70836a778120b9de34fed7be06b03a
Assassin-1234 <chordsandfrets@gmail.com>
2021-04-11 17:47:14 +0530

    functions and mongodb

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-require('dotenv').config()
-const bodyParser = require('body-parser')
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express()
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.mongouri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 app.set('json spaces', 1);
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/arc-sw.js', function (req, res) {
-	res.sendFile('/arc-sw.js', {root: './routes'})
+app.get('/arc-sw.js', function(req, res) {
+	res.sendFile('/arc-sw.js', { root: './routes' });
 });
 
-app.listen(3000)
-console.log('Server is now ready on port: 3000')
+app.listen(3000);
+console.log('Server is now ready on port: 3000');
